5dd45a0b1b8b3b8ca25cf737d5c664a6bcc4413f
Kaveh HajiNajaf <kavehhn174@gmail.com>
2022-07-29 20:07:14 +0430

    Started project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.get("/", function(req,res) {
+    res.send()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen('3000', function () {
+    console.log("Server started...")
+})
