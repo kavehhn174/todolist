3d7ba57174da4ac8cf70ad9de3e18e35fa219835
Kaveh HajiNajaf <kavehhn174@gmail.com>
2022-07-30 01:04:11 +0430

    Added script to ejs files The tasks now get listed properly Animations added to textbox and cards

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+let tasks = ["Go Shopping", "Do Homework" , "Drink Coffee"];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
@@ -17,13 +19,17 @@ app.get("/", function(req,res) {
         weekday: "long" , day: "numeric", month:"long"
     }
     let day = today.toLocaleDateString("en-US", options);
-    res.render("home", {day: day});
+    res.render("home", {day: day, newTasksItems: tasks});
 
 })
 
-
-
-
+app.post("/", function (req,res) {
+    const newTask = req.body.newTask;
+    if (newTask !== "") {
+        tasks.push(newTask);
+        res.redirect('/')
+    }
+})
 
 app.listen('3000', function () {
     console.log("Server started...")
