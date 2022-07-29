c4b2be8f30589c622296c90f6a2e2a56999ba41d
Kaveh HajiNajaf <kavehhn174@gmail.com>
2022-07-29 20:33:24 +0430

    added ejs home file and tested ejs

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.get("/", function(req,res) {
-    res.send()
+    let today = new Date();
+
+    res.render("home", {today: today});
+
 })
-
-
-
-
-
-
-
 
 
 
